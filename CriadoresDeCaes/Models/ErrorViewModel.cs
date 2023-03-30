42d6d0cd92839ce8f38fcbd1cc176090186f5bed
Ridennator <rlcpm@hotmail.com>
2023-03-30 15:18:17 +0100

    init commit

---
+++ CriadoresDeCaes/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CriadoresDeCaes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
